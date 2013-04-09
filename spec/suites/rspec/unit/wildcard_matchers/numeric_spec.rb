b0036117bbb8a0284ffe59e932f25eb9e5459c65
Elliot Winkler <elliot.winkler@gmail.com>
2013-04-09 00:20:17 -0600

    Refactor #numeric and its tests

--- spec/suites/rspec/unit/wildcard_matchers/numeric_spec.rb
+++ spec/suites/rspec/unit/wildcard_matchers/numeric_spec.rb
@@ -3,27 +3,41 @@ require File.expand_path("#{File.dirname(__FILE__)}/../../spec_helper")
 module RR
   module WildcardMatchers
     describe Numeric do
-      attr_reader :matcher
-      before do
-        @matcher = Numeric.new
-      end
+      include WildcardMatcherMatchers
 
-      describe "#wildcard_match?" do
-        context "when passed a Numeric" do
-          it "returns true" do
-            expect(matcher).to be_wildcard_match(99)
-          end
+      describe '#wildcard_match?' do
+        it "returns true when given Numeric is a copy of this Numeric" do
+          matcher2 = described_class.new
+          should wildcard_match(matcher2)
         end
 
-        context "when not passed a Numeric" do
-          it "returns false" do
-            matcher.should_not be_wildcard_match(:not_a_numeric)
-          end
+        it "returns true when given a Numeric" do
+          should wildcard_match(99)
+        end
+
+        it "returns false otherwise" do
+          should_not wildcard_match(:not_a_numeric)
+        end
+      end
+
+      describe '#==' do
+        it "returns true when given Numeric is a copy of this Numeric" do
+          matcher2 = described_class.new
+          should equal_match(matcher2)
+        end
+
+        it "returns false even when given an object that wildcard matches this Numeric" do
+          should_not equal_match(99)
+        end
+
+        it "returns false when not a Numeric whatsoever" do
+          should_not equal_match(:something_else)
         end
       end
 
       describe "#inspect" do
-        it "returns numeric" do
+        it "returns the correct string" do
+          matcher = described_class.new
           expect(matcher.inspect).to eq "numeric"
         end
       end
