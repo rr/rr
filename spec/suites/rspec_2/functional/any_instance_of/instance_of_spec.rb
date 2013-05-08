e82ca057b5fcd6a8a0b67d2fb9f08711304a1ca6
Elliot Winkler <elliot.winkler@gmail.com>
2013-05-08 00:19:39 -0600

    Merge the all_instance_of test with the instance_of test

--- spec/suites/rspec_2/functional/any_instance_of/instance_of_spec.rb
+++ spec/suites/rspec_2/functional/any_instance_of/instance_of_spec.rb
@@ -1,12 +1,14 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../../spec_helper")
 
-describe "instance_of" do
-  it "applies to instances instantiated before the Double expection was created" do
-    subject_class = Class.new
-    subject = subject_class.new
-    instance_of(subject_class) do |o|
-      o.to_s {"Subject is stubbed"}
+%w(instance_of all_instances_of).each do |method|
+  describe method do
+    it "applies to instances instantiated before the Double expection was created" do
+      subject_class = Class.new
+      subject = subject_class.new
+      __send__(method, subject_class) do |o|
+        o.to_s {"Subject is stubbed"}
+      end
+      expect(subject.to_s).to eq "Subject is stubbed"
     end
-    expect(subject.to_s).to eq "Subject is stubbed"
   end
 end
