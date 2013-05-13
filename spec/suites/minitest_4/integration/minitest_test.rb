44d6a9fc6cfe719ee56ea67af3d9936c51bb9507
Elliot Winkler <elliot.winkler@gmail.com>
2013-05-12 21:38:04 -0600

    Don't need the minitest_4 suite as we can re-use the minitest one

--- spec/suites/minitest_4/integration/minitest_test.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../../test_helper', __FILE__)
-require File.expand_path('../../../common/test_unit_tests', __FILE__)
-
-class MiniTestIntegrationTest < MiniTest::Unit::TestCase
-  # Test::Unit compatibility
-  alias_method :assert_raise, :assert_raises
-
-  include TestUnitTests
-end
--- spec/suites/minitest_4/test_helper.rb
+++
@@ -1,3 +0,0 @@
-require 'minitest/autorun'
-
-require File.expand_path('../../../global_helper', __FILE__)
