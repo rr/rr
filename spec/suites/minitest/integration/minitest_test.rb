57b2ed52718c41b695df582460061c403806e629
Elliot Winkler <elliot.winkler@gmail.com>
2013-05-12 21:37:21 -0600

    minitest_test tests both 4 and 5

--- spec/suites/minitest/integration/minitest_test.rb
+++ spec/suites/minitest/integration/minitest_test.rb
@@ -1,7 +1,11 @@
 require File.expand_path('../../test_helper', __FILE__)
 require File.expand_path('../../../common/test_unit_tests', __FILE__)
 
-class MiniTestIntegrationTest < Minitest::Test
+test_case_superclass = (defined?(Minitest) && defined?(Minitest::VERSION)) ? \
+  Minitest::Test : \
+  MiniTest::Unit::TestCase
+
+class MiniTestIntegrationTest < test_case_superclass
   # Test::Unit compatibility
   alias_method :assert_raise, :assert_raises
 
