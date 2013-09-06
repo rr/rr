168abcfc363d1565858d1a65fb6f8b585bd270c9
Elliot Winkler <elliot.winkler@gmail.com>
2013-09-05 23:12:36 -0600

    Add remaining tests for #spy

--- spec/suites/rspec_2/functional/spy_spec.rb
+++ spec/suites/rspec_2/functional/spy_spec.rb
@@ -64,4 +64,46 @@ describe 'spy' do
     subject.respond_to?(:foo)
     subject.should_not have_received(:respond_to?)
   end
+
+  it "excludes #methods from the list of recorded methods" do
+    subject = Object.new
+    spy(subject)
+    subject.methods
+    subject.should_not have_received.methods
+  end
+
+  it "excludes #== from the list of recorded methods" do
+    subject = Object.new
+    spy(subject)
+    subject == 5
+    subject.should_not have_received(:==)
+  end
+
+  it "excludes #__send__ from the list of recorded methods" do
+    subject = Object.new
+    spy(subject)
+    subject.__send__('to_s')
+    subject.should_not have_received(:__send__)
+  end
+
+  it "excludes #__id__ from the list of recorded methods" do
+    subject = Object.new
+    spy(subject)
+    subject.__id__
+    subject.should_not have_received(:__id__)
+  end
+
+  it "excludes #object_id from the list of recorded methods" do
+    subject = Object.new
+    spy(subject)
+    subject.object_id
+    subject.should_not have_received(:object_id)
+  end
+
+  it "excludes #class from the list of recorded methods" do
+    subject = Object.new
+    spy(subject)
+    subject.class
+    subject.should_not have_received.class
+  end
 end
