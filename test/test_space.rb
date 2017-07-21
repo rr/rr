fbc81c7e4312a2502c95f42ce6c4d813a411f370
Kouhei Sutou <kou@clear-code.com>
2017-07-21 23:39:10 +0900

    Add mote tests

--- test/test_space.rb
+++ test/test_space.rb
@@ -1,38 +1,128 @@
 class TestSpace < Test::Unit::TestCase
   sub_test_case "#reset" do
-    test "respond_to?: instance method" do
-      subject = Object.new
-      stub(subject).exist? {true}
-      assert do
-        subject.exist?
+    sub_test_case "existent" do
+      test "respond_to?: instance method" do
+        subject = Object.new
+        def subject.hello
+          :original_hello
+        end
+        stub(subject).hello {:stub_hello}
+        assert_equal(:stub_hello, subject.hello)
+        assert do
+          subject.respond_to?(:hello)
+        end
+        RR.reset
+        assert_equal(:original_hello, subject.hello)
+        assert do
+          subject.respond_to?(:hello)
+        end
       end
-      assert do
-        subject.respond_to?(:exist?)
-      end
-      RR.reset
-      assert_raise(NoMethodError) do
-        subject.exist?
-      end
-      assert do
-        not subject.respond_to?(:exist?)
+
+      test "respond_to?: class method" do
+        subject = Class.new do
+          def self.hello
+            :original_hello
+          end
+        end
+        stub(subject).hello {:stub_hello}
+        assert_equal(:stub_hello, subject.hello)
+        assert do
+          subject.respond_to?(:hello)
+        end
+        RR.reset
+        assert_equal(:original_hello, subject.hello)
+        assert do
+          subject.respond_to?(:hello)
+        end
       end
     end
 
-    test "respond_to?: class method" do
-      subject = Class.new
-      stub(subject).exist? {true}
-      assert do
-        subject.exist?
+    sub_test_case "nonexistent" do
+      test "respond_to?: instance method" do
+        subject = Object.new
+        stub(subject).stub_predicate? {true}
+        assert do
+          subject.stub_predicate?
+        end
+        assert do
+          subject.respond_to?(:stub_predicate?)
+        end
+        RR.reset
+        assert_raise(NoMethodError) do
+          subject.stub_predicate?
+        end
+        assert do
+          not subject.respond_to?(:stub_predicate?)
+        end
       end
-      assert do
-        subject.respond_to?(:exist?)
+
+      test "respond_to?: class method" do
+        subject = Class.new
+        stub(subject).stub_predicate? {true}
+        assert do
+          subject.stub_predicate?
+        end
+        assert do
+          subject.respond_to?(:stub_predicate?)
+        end
+        RR.reset
+        assert_raise(NoMethodError) do
+          subject.stub_predicate?
+        end
+        assert do
+          not subject.respond_to?(:stub_predicate?)
+        end
       end
-      RR.reset
-      assert_raise(NoMethodError) do
-        subject.exist?
+    end
+
+    sub_test_case "lazy defined" do
+      test "respond_to?: instance method" do
+        subject = Object.new
+        def subject.method_missing(method_name, *args, &block)
+          if method_name.to_sym == :hello
+            def self.hello
+              :original_hello
+            end
+            hello
+          else
+            super
+          end
+        end
+        stub(subject).hello {:stub_hello}
+        assert_equal(:stub_hello, subject.hello)
+        assert do
+          subject.respond_to?(:hello)
+        end
+        RR.reset
+        assert_equal(:original_hello, subject.hello)
+        assert do
+          subject.respond_to?(:hello)
+        end
       end
-      assert do
-        not subject.respond_to?(:exist?)
+
+      test "respond_to?: class method" do
+        subject = Class.new do
+          def self.method_missing(method_name, *args, &block)
+            if method_name.to_sym == :hello
+              def self.hello
+                :original_hello
+              end
+              hello
+            else
+              super
+            end
+          end
+        end
+        stub(subject).hello {:stub_hello}
+        assert_equal(:stub_hello, subject.hello)
+        assert do
+          subject.respond_to?(:hello)
+        end
+        RR.reset
+        assert_equal(:original_hello, subject.hello)
+        assert do
+          subject.respond_to?(:hello)
+        end
       end
     end
   end
