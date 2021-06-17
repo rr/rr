23f6a8737da5ed265bf7659adfde79fded41611c
Sutou Kouhei <kou@clear-code.com>
2021-06-18 08:52:34 +0900

    mock test: add ordered arguments + keyword arguments case

--- test/mock/test_proxy.rb
+++ test/mock/test_proxy.rb
@@ -7,4 +7,13 @@ class TestMockProxy < Test::Unit::TestCase
     mock.proxy(object).hello(name: "Bob").once
     object.hello(name: "Bob")
   end
+
+  test "ordered arguments + keyword arguments" do
+    object = Object.new
+    def object.hello(name, comment: "Yay!")
+      "Hello #{name}! #{comment}."
+    end
+    mock.proxy(object).hello("Bob", comment: "Wow!").once
+    object.hello("Bob", comment: "Wow!")
+  end
 end
