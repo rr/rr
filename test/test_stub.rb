c36e301e458ffe05ac486b2495e7cb299644e744
Sutou Kouhei <kou@clear-code.com>
2021-08-07 06:16:06 +0900

    test stub: remove duplicated tests

--- test/test_stub.rb
+++ test/test_stub.rb
@@ -1,22 +1,19 @@
 class TestStub < Test::Unit::TestCase
-  test "ordered arguments + keyword arguments" do
-    object = Object.new
-    stub(object).hello("Alice", "Bob", comment: "Wow!").once
-    object.hello("Alice", "Bob", comment: "Wow!")
-  end
-
-  test "keyword arguments expectation" do
-    object = Object.new
-    stub(object).call(1, b: 2).once
-    object.call(1, b: 2)
-  end
-
-  test "keyword arguments expectation with implementation" do
-    klass = Class.new do
-      def call(a, b:); end
+  sub_test_case("ordered arguments + keyword arguments") do
+    test("without implementation") do
+      object = Object.new
+      stub(object).hello("Alice", "Bob", comment: "Wow!").once
+      object.hello("Alice", "Bob", comment: "Wow!")
     end
-    object = klass.new
-    stub(object).call(1, b: 2).once
-    object.call(1, b: 2)
+
+    test("with implementation") do
+      klass = Class.new do
+        def hello(from, to, comment:)
+        end
+      end
+      object = klass.new
+      stub(object).hello("Alice", "Bob", comment: "Wow!").once
+      object.hello("Alice", "Bob", comment: "Wow!")
+    end
   end
 end
